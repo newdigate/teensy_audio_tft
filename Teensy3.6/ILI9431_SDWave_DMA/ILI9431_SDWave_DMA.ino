e272aecf1718660c380d65bcb5f9119c0b0c8653
Nic Newdigate <nicnewdigate@gmail.com>
2016-10-30 17:49:02 +0000

    Create ILI9431_SDWave_DMA.ino

---
+++ Teensy3.6/ILI9431_SDWave_DMA/ILI9431_SDWave_DMA.ino
@@ -0,0 +1,292 @@
+/***************************************************
+  This is our GFX example for the Adafruit ILI9341 Breakout and Shield
+  ----> http://www.adafruit.com/products/1651
+
+  Check out the links above for our tutorials and wiring diagrams
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional)
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <SD.h>
+#include <Audio.h>
+#include "SPI.h"
+#include <ILI9341_t3DMA.h>
+
+#define SDCARD_CS_PIN    BUILTIN_SDCARD
+
+#define TFT_DC      15
+#define TFT_CS      10
+#define TFT_RST     4  // 255 = unused, connect to 3.3V
+#define TFT_MOSI    11
+#define TFT_SCLK    13
+#define TFT_MISO    12
+
+ILI9341_t3DMA tft = ILI9341_t3DMA(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK, TFT_MISO);
+#define BUFFPIXEL 80
+
+AudioPlaySdWav           playWav1;
+// Use one of these 3 output types: Digital I2S, Digital S/PDIF, or Analog DAC
+//AudioOutputI2S           audioOutput;
+//AudioOutputSPDIF       audioOutput;
+AudioOutputAnalog      audioOutput;
+AudioConnection          patchCord1(playWav1, 0, audioOutput, 0);
+AudioConnection          patchCord2(playWav1, 1, audioOutput, 1);
+AudioRecordQueue         queue1;         //xy=598,180
+AudioConnection          patchCord3(playWav1, 0, queue1, 0);
+
+
+void setup() {
+
+  // Audio connections require memory to work.  For more
+  // detailed information, see the MemoryAndCpuUsage example
+  AudioMemory(8);
+  
+  tft.begin();
+
+  //START DMA MODE
+  tft.refresh();
+    
+  tft.dfillScreen(ILI9341_BLACK);
+  tft.setTextColor(ILI9341_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Waiting for Arduino Serial Monitor...");
+  tft.refreshOnce();  
+  Serial.begin(9600);
+  while (!Serial) ; // wait for Arduino Serial Monitor
+
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    // stop here, but print a message repetitively
+    while (1) {
+      Serial.println("Unable to access the SD card");
+      delay(500);
+    }
+  }
+
+  //tft.stopRefresh();
+
+  bmpDraw("parrot.bmp", 0, 0);
+    
+  queue1.begin();
+}
+
+uint16_t bkgImage[64][128];
+
+void loop(void) {
+  Serial.println("ILI9341 Test!"); 
+  playFile("Dead_Combo.wav");
+}
+
+//===========================================================
+// Try Draw using writeRect
+void bmpDraw(const char *filename, uint8_t x, uint16_t y) {
+
+  File     bmpFile;
+  int      bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t bmpImageoffset;        // Start of image data in file
+  uint32_t rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
+  uint16_t buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+  boolean  goodBmp = false;       // Set to true on valid header parse
+  boolean  flip    = true;        // BMP is stored bottom-to-top
+  int      w, h, row, col;
+  uint8_t  r, g, b;
+  uint32_t pos = 0, startTime = millis();
+
+  uint16_t awColors[320];  // hold colors for one row at a time...
+
+  if((x >= tft.width()) || (y >= tft.height())) return;
+
+  Serial.println();
+  Serial.print(F("Loading image '"));
+  Serial.print(filename);
+  Serial.println('\'');
+
+  // Open requested file on SD card
+  if (!(bmpFile = SD.open(filename))) {
+    Serial.print(F("File not found"));
+    return;
+  }
+
+  // Parse BMP header
+  if(read16(bmpFile) == 0x4D42) { // BMP signature
+    Serial.print(F("File size: ")); Serial.println(read32(bmpFile));
+    (void)read32(bmpFile); // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile); // Start of image data
+    Serial.print(F("Image Offset: ")); Serial.println(bmpImageoffset, DEC);
+    // Read DIB header
+    Serial.print(F("Header size: ")); Serial.println(read32(bmpFile));
+    bmpWidth  = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+    if(read16(bmpFile) == 1) { // # planes -- must be '1'
+      bmpDepth = read16(bmpFile); // bits per pixel
+      Serial.print(F("Bit Depth: ")); Serial.println(bmpDepth);
+      if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
+
+        goodBmp = true; // Supported BMP format -- proceed!
+        Serial.print(F("Image size: "));
+        Serial.print(bmpWidth);
+        Serial.print('x');
+        Serial.println(bmpHeight);
+
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * 3 + 3) & ~3;
+
+        // If bmpHeight is negative, image is in top-down order.
+        // This is not canon but has been observed in the wild.
+        if(bmpHeight < 0) {
+          bmpHeight = -bmpHeight;
+          flip      = false;
+        }
+
+        // Crop area to be loaded
+        w = bmpWidth;
+        h = bmpHeight;
+        if((x+w-1) >= tft.width())  w = tft.width()  - x;
+        if((y+h-1) >= tft.height()) h = tft.height() - y;
+ Serial.print(w, h);
+        for (row=0; row<h; row++) { // For each scanline...
+          //Serial.printf("%n %n", col, row);
+          // Seek to start of scan line.  It might seem labor-
+          // intensive to be doing this on every line, but this
+          // method covers a lot of gritty details like cropping
+          // and scanline padding.  Also, the seek only takes
+          // place if the file position actually needs to change
+          // (avoids a lot of cluster math in SD library).
+          if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
+            pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+          else     // Bitmap is stored top-to-bottom
+            pos = bmpImageoffset + row * rowSize;
+          if(bmpFile.position() != pos) { // Need seek?
+            bmpFile.seek(pos);
+            buffidx = sizeof(sdbuffer); // Force buffer reload
+          }
+
+          for (col=0; col<w; col++) { // For each pixel...
+            // Time to read more pixel data?
+            if (buffidx >= sizeof(sdbuffer)) { // Indeed
+              bmpFile.read(sdbuffer, sizeof(sdbuffer));
+              buffidx = 0; // Set index to beginning
+            }
+
+            // Convert pixel from BMP to TFT format, push to display
+            b = sdbuffer[buffidx++];
+            g = sdbuffer[buffidx++];
+            r = sdbuffer[buffidx++];
+            //Serial.print("nn");
+            bkgImage[row][col] = tft.color565(r,g,b);
+            //awColors[col] = tft.color565(r,g,b);
+            //Serial.print(awColors[col]);
+            //Serial.print(" ");
+            //tft.ddrawPixel(col, row, awColors[col]);
+            //tft.ddrawPixel(col, row, ILI9341_BLACK);            
+            
+          } // end pixel
+          tft.dwriteRect(0, row, w, 1, awColors);
+          //Serial.println();
+        } // end scanline
+        Serial.print(F("Loaded in "));
+        Serial.print(millis() - startTime);
+        Serial.println(" ms");
+      } // end goodBmp
+    }
+  }
+
+  bmpFile.close();
+  if(!goodBmp) Serial.println(F("BMP format not recognized."));
+  delay(1000);
+}
+
+
+
+// These read 16- and 32-bit types from the SD card file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+
+uint16_t read16(File &f) {
+  uint16_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
+  return result;
+}
+
+uint32_t read32(File &f) {
+  uint32_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read();
+  ((uint8_t *)&result)[2] = f.read();
+  ((uint8_t *)&result)[3] = f.read(); // MSB
+  return result;
+}
+
+void playFile(const char *filename)
+{
+  Serial.print("Playing file: ");
+  Serial.println(filename);
+
+  // Start playing the file.  This sketch continues to
+  // run while the file plays.
+  playWav1.play(filename);
+
+  // A brief delay for the library read WAV info
+  delay(5);
+
+  // Simply wait for the file to finish playing.
+  while (playWav1.isPlaying()) {
+    if (queue1.available()) {
+      int16_t *buf = queue1.readBuffer();
+       
+      tft.stopRefresh();  
+      //unsigned long start = micros();
+      //tft.dwriteRect(0,0, 128, 64, &bkgImage[0][0]);
+      tft.dwriteRectScale(0,0, 128, 64, 240, 320, &bkgImage[0][0]);
+      //unsigned long duration = micros() - start;
+      //Serial.println(duration); 
+      
+      //for (int k =0; k<128; k++) {
+      //  tft.ddrawPixel(k, buf[k], ILI9341_GREEN);  
+      //}
+      for (int k =1; k<128; k++) {
+        uint16_t y = buf[k] / 256;
+        uint16_t old_y = buf[k-1] / 256;
+        uint16_t x = (240 * k)  / 128;
+        uint16_t old_x = (240 * (k-1)) / 128;
+        //tft.ddrawLine(old_x, old_y+160, x, y+160, ILI9341_BLACK);  
+        drawLine(old_x, old_y+160, x, y+160, ILI9341_BLACK);  
+      }          
+      tft.refresh();    
+      //tft.refreshOnce();
+      queue1.freeBuffer();
+
+    } else
+    delay(1);   
+  }
+}
+
+void drawLine( uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color)
+{
+    int x = x1-x0;
+    int y = y1-y0;
+    int dx = abs(x), sx = x0<x1 ? 1 : -1;
+    int dy = -abs(y), sy = y0<y1 ? 1 : -1;
+    int err = dx+dy, e2;                                                /* error value e_xy             */
+    for (;;){                                                           /* loop                         */
+        tft.ddrawPixel(x0,y0,color);
+        e2 = 2*err;
+        if (e2 >= dy) {                                                 /* e_xy+e_x > 0                 */
+            if (x0 == x1) break;
+            err += dy; x0 += sx;
+        }
+        if (e2 <= dx) {                                                 /* e_xy+e_y < 0                 */
+            if (y0 == y1) break;
+            err += dx; y0 += sy;
+        }
+    }
+}
+
